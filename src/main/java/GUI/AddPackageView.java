7f7ec910fe87f714fe072e6d724925e5bf4a138b
Florian <Florian@Florian-PC1337.fritz.box>
2021-06-28 13:40:41 +0200

    AddPackageView

--- src/main/java/GUI/AddPackageView.java
+++ src/main/java/GUI/AddPackageView.java
@@ -1,8 +1,9 @@
 package GUI;
 
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
-public class AddPackageView extends BorderPane {
+public class AddPackageView extends VBox {
 
     public AddPackageView(){
         
